97bb0b13e979a3b96468bcbc8b856a11190a34ec
y <82239261+Yanis299@users.noreply.github.com>
2023-06-11 21:41:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "token": "MTEwODQwNjA0NDc3MTc2MjI0Ng.Gnlotn.aNnTBWIZuevyV1HFHUD0kURBbhiz_bNG02bLWM",
+  "token": "",
   "moniteur": {
     "interval": 5000,
     "debug": false,
@@ -7,7 +7,7 @@ module.exports = {
       {
         "id": "cfa4f",
         "url": "https://www.vinted.fr/catalog?search_text=nike&search_id=9846357662&order=newest_first",
-        "salon": "1111362921822429266"
+        "salon": ""
       },
 
     ]
